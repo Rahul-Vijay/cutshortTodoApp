da06435c6899d81a8c73e47afb315d5c078ef19e
Rahul-Vijay <vk.rahul318@gmail.com>
2023-02-13 20:40:46 +0530

    establish db

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
+import db from "@todoapp/utils/db";
 import { createServer } from "./utils/server";
 
-createServer()
-  .then((server) => {
-    server.listen(3000, () => {
-      console.info(`Listening on http://localhost:3000`);
-    });
-  })
+db.open()
+  .then(() =>
+    createServer().then((server) => {
+      server.listen(3000, () => {
+        console.info(`Listening on http://localhost:3000`);
+      });
+    })
+  )
   .catch((err) => {
     console.error(`Error: ${err}`);
   });
---
+++ src/utils/db.ts
@@ -0,0 +1,67 @@
+/* istanbul ignore file */
+
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+mongoose.Promise = global.Promise;
+mongoose.set("debug", process.env.DEBUG !== undefined);
+
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  keepAlive: true,
+  keepAliveInitialDelay: 300000,
+  autoIndex: true,
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+};
+
+class MongoConnection {
+  private static _instance: MongoConnection;
+
+  private _mongoServer?: MongoMemoryServer;
+
+  static getInstance(): MongoConnection {
+    if (!MongoConnection._instance) {
+      MongoConnection._instance = new MongoConnection();
+    }
+    return MongoConnection._instance;
+  }
+
+  public async open(): Promise<void> {
+    try {
+      console.log("connecting to inmemory mongo db");
+      this._mongoServer = await MongoMemoryServer.create();
+      const mongoUrl = this._mongoServer.getUri();
+      await mongoose.connect(mongoUrl, opts);
+
+      mongoose.connection.on("connected", () => {
+        console.info("Mongo: connected");
+      });
+
+      mongoose.connection.on("disconnected", () => {
+        console.error("Mongo: disconnected");
+      });
+
+      mongoose.connection.on("error", (err) => {
+        console.error(`Mongo:  ${String(err)}`);
+      });
+    } catch (err) {
+      console.error(`db.open: ${err}`);
+      throw err;
+    }
+  }
+
+  public async close(): Promise<void> {
+    try {
+      await mongoose.disconnect();
+      await this._mongoServer!.stop();
+    } catch (err) {
+      console.error(`db.open: ${err}`);
+      throw err;
+    }
+  }
+}
+
+export default MongoConnection.getInstance();

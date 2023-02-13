24a8d91916170c3a063e5afce64c7e1f1df96b07
Rahul-Vijay <vk.rahul318@gmail.com>
2023-02-13 20:08:25 +0530

    first test

--- src/api/controllers/__tests__/todo.ts
+++ src/api/controllers/__tests__/todo.ts
@@ -1,10 +1,29 @@
 import request from "supertest";
 import { Express } from "express-serve-static-core";
 
-// import {createServer} from ''
+import { createServer } from "@todoapp/utils/server";
 
 let server: Express;
 
 beforeAll(async () => {
-  //   server = await createServer()
+  server = await createServer();
+});
+
+describe("GET /todo", () => {
+  it("should return 200 & valid response if request param list is empity", (done) => {
+    request(server)
+      .get(`/api/v1/todo`)
+      .set("Authorization", "Bearer fakeToken")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        expect(res.body).toMatchObject({
+          message: "Hello, 12345, with fakeUserId!",
+        });
+        done();
+      });
+  });
 });

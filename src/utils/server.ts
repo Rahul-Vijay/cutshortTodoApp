ee9a2fe3fe711bee2de8d65c77b5a632f1e3fe8b
Rahul-Vijay <vk.rahul318@gmail.com>
2023-02-13 13:18:40 +0530

    setup server

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,45 @@
 import express from "express";
 import { Express } from "express-serve-static-core";
+import { connector, summarise } from "swagger-routes-express";
+import YAML from "yamljs";
+
+import * as api from "../api/controllers";
 
 export async function createServer(): Promise<Express> {
+  const yamlSpecFile = "./config/openapi.yml";
+  const apiDefinition = YAML.load(yamlSpecFile);
+  const apiSummary = summarise(apiDefinition);
+  console.info(apiSummary);
+
   const server = express();
-  server.get("/", (req, res) => {
-    res.send("Hello world!!!");
+
+  // error customization, if request is invalid
+  server.use(
+    (
+      err: any,
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      res.status(err.status).json({
+        error: {
+          type: "request_validation",
+          message: err.message,
+          errors: err.errors,
+        },
+      });
+    }
+  );
+
+  const connect = connector(api, apiDefinition, {
+    onCreateRoute: (method: string, descriptor: any[]) => {
+      console.log(
+        `${method}: ${descriptor[0]} : ${(descriptor[1] as any).name}`
+      );
+    },
   });
+
+  connect(server);
+
   return server;
 }

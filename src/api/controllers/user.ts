f0657ff56ce9f36f1777b9389e6738c53883173f
Rahul-Vijay <vk.rahul318@gmail.com>
2023-02-17 17:43:28 +0530

    login and create user

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 
 import UserService from "@todoapp/api/services/user";
 import { writeJsonResponse } from "@todoapp/utils/express";
+import { ErrorResponse } from "../interfaces/interfaces";
 
 export function auth(
   req: express.Request,
@@ -22,6 +23,68 @@ export function auth(
     })
     .catch((err) => {
       console.log(err);
+      writeJsonResponse(res, 500, {
+        error: {
+          type: "internal_server_error",
+          message: "Internal Server Error",
+        },
+      });
+    });
+}
+
+export function login(req: express.Request, res: express.Response): void {
+  const { email, password } = req.body;
+
+  UserService.login(email, password)
+    .then((resp) => {
+      if ((resp as any).error) {
+        if ((resp as ErrorResponse).error.type === "invalid_credentials") {
+          writeJsonResponse(res, 404, resp);
+        } else {
+          throw new Error(`unsupported ${resp}`);
+        }
+      } else {
+        const { userId, token, expireAt } = resp as {
+          token: string;
+          userId: string;
+          expireAt: Date;
+        };
+        writeJsonResponse(
+          res,
+          200,
+          { userId: userId, token: token },
+          { "X-Expires-After": expireAt.toISOString() }
+        );
+      }
+    })
+    .catch((err: any) => {
+      console.error(`login: ${err}`);
+      writeJsonResponse(res, 500, {
+        error: {
+          type: "internal_server_error",
+          message: "Internal Server Error",
+        },
+      });
+    });
+}
+
+export function createUser(req: express.Request, res: express.Response): void {
+  const { email, password, name } = req.body;
+
+  UserService.createUser(email, password, name)
+    .then((resp) => {
+      if ((resp as any).error) {
+        if ((resp as ErrorResponse).error.type === "account_already_exists") {
+          writeJsonResponse(res, 409, resp);
+        } else {
+          throw new Error(`unsupported ${resp}`);
+        }
+      } else {
+        writeJsonResponse(res, 201, resp);
+      }
+    })
+    .catch((err: any) => {
+      console.error(`createUser: ${err}`);
       writeJsonResponse(res, 500, {
         error: {
           type: "internal_server_error",
